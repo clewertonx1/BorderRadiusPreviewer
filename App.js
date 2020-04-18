0130921f4e7bd3564913e74f55609303e59cd089
Clewerton Roque <clewerton203@gmail.com>
2020-04-18 18:21:37 -0300

    Project Finished

--- App.js
+++ App.js
@@ -1,27 +1,62 @@
 import * as React from 'react';
-import {StyleSheet, Text, View, TextInput } from 'react-native';
+import { useState } from 'react'
+import {StyleSheet, Text, View, TextInput, TouchableOpacity, Clipboard  } from 'react-native';
+
 
 export default function App() {
+
+  const [borderTopLeft, setBorderTopLeft] = useState(0)
+  const [borderTopRight, setBorderTopRight] = useState(0)
+  const [borderBottomRight, setBorderBottomRight] = useState(0)
+  const [borderBottomLeft, setBorderBottomLeft] = useState(0)
+
   return (
     <View style={styles.container}>
+      {/* Title*/}
       <Text style={styles.title}>Border Radius Previewer</Text>
+      {/*Form and inputs*/}  
+      <View style={{margin: 20, flexDirection: "row"}}>  
 
-      <View style={styles.a}>  
-
-        <View style={styles.inputsView}>
-          <TextInput style={[styles.input]}></TextInput>         
-          <TextInput style={[styles.input]}></TextInput>    
+        {/* Input View Left*/}
+        <View style={styles.inputView}>
+          <TextInput 
+            style={styles.input} keyboardType={'numeric'} 
+            onChangeText={text =>  text ? setBorderTopLeft(parseInt(text)) : console.log("value invalid")}>
+          </TextInput>
+          <TextInput 
+            style={styles.input} keyboardType={'numeric'} 
+            onChangeText={text =>  text ? setBorderBottomLeft(parseInt(text)) : console.log("value invalid")}>
+          </TextInput>
         </View>
 
-        <View style={[styles.form]}></View> 
+        {/* Form square*/}
+        <View style={[styles.form, {borderTopRightRadius: borderTopRight, borderTopLeftRadius: borderTopLeft, borderBottomRightRadius: borderBottomRight, borderBottomLeftRadius: borderBottomLeft }]}/>
+        
+         {/* Input View Right*/}
+        <View style={styles.inputView}>
+        <TextInput 
+            style={styles.input} keyboardType={'numeric'}  
+            onChangeText={text =>  text ? setBorderTopRight(parseInt(text)) : console.log("value invalid")}>
+          </TextInput>
 
-        <View style={styles.inputsView}>
-          <TextInput style={[styles.input]}></TextInput> 
-          <TextInput style={[styles.input]}></TextInput> 
+          <TextInput 
+            style={styles.input} keyboardType={'numeric'} 
+            onChangeText={text =>  text ? setBorderBottomRight(parseInt(text)) : console.log("value invalid")}>
+          </TextInput>
         </View>
-
       </View>
-
+      {/* Button to copy to clipboard*/}
+      <TouchableOpacity style={styles.button} 
+        onPress={() => { 
+          Clipboard.setString(
+            'borderTopLeftRadius: ' + borderTopLeft 
+            + ', borderBottomLeftRadius: ' + borderBottomLeft 
+            + ', borderTopRightRadius: ' + borderTopRight 
+            + ', borderBottomRightRadius: ' + borderBottomRight)
+          alert("Copied to clipboard")
+        }}> 
+        <Text>Copy to clipborad</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -31,11 +66,11 @@ const styles = StyleSheet.create({
      flex: 1, 
      justifyContent: 'center',
      alignItems: 'center',
-    backgroundColor: '#212121',
+     backgroundColor: '#212121',
   },
   title: {
+   
     fontSize: 20,
-    justifyContent: 'flex-start',
     textAlign: 'center',
     margin: 10,
     color: '#59dbff',
@@ -55,14 +90,20 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: "#606060", 
   },
-  a:{
-    flexDirection: "row",
-  },
-  inputsView: {
+  inputView: {
     marginHorizontal: 10,
     justifyContent: 'space-between',
   
   },
+  button:{ 
+    margin: 20,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#59dbff',
+    width: 140,
+    height:30,
+    borderRadius: 10,
+  }
 
 
 });
